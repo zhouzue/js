aecda80246c7f894cc7b1927fd1f3ac698176150
zhouzue <964190513z@gmail.com>
2022-07-04 15:04:03 +0800

    1
    1

---
+++ 701/12.9/main.js
@@ -0,0 +1,6 @@
+var i=0;
+while(i<5){
+    i++;
+    console.log(i);
+}
+console.log('no i')
---
+++ 701/lern/main.js
@@ -0,0 +1,41 @@
+var isadmin = false;
+if(isadmin){
+    console.log("he isadmin")
+}else{
+    console.log('he not is')
+}
+
+var day="";
+switch(day){
+    case 1 :
+    console.log('food')
+    break;
+    case 2 :
+    console.log('water')
+    break;
+    case 3 :
+    console.log('apple')
+    break;
+    default:
+    console.log('yeyeye')
+    break;
+}
+
+var sister = 10;
+sister += 2 ;
+console.log('sitster:',sister)
+
+console.log(1>2)
+
+var he =false,
+    she =false ;
+if(he && she ){
+    console.log("good")
+}else{
+    console.log("bad")
+}
+
+var i ;
+for(i=1; i<=100; i++){
+    console.log(i);
+}
